0fddd82ca22194e237b04ced8ddcb0a7191b2073
Alex Garrett-Smith <alex@codecourse.com>
2024-06-06 13:17:09 +0100

    Episode 19

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,17 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Config\Config;
 use Laminas\Diactoros\Request;
 use Laminas\Diactoros\Response;
 use Psr\Http\Message\ServerRequestInterface;
 
 class HomeController
 {
+    public function __construct(
+        protected Config $config
+    ) {}
+
     public function __invoke(ServerRequestInterface $request)
     {
         $response = new Response();
 
-        $response->getBody()->write('Home');
+        $response->getBody()->write($this->config->get('app.name'));
 
         return $response;
     }
