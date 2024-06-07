ca2c854709253813df842d624a70ab2ea9b9d023
Alex Garrett-Smith <alex@codecourse.com>
2024-06-07 14:53:31 +0100

    Episode 39

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -8,12 +8,16 @@ use Cartalyst\Sentinel\Sentinel;
 use Laminas\Diactoros\Request;
 use Laminas\Diactoros\Response;
 use Psr\Http\Message\ServerRequestInterface;
+use Respect\Validation\Exceptions\ValidatorException;
+use Respect\Validation\Validator as v;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class LoginController
 {
     public function __construct(
         protected View $view,
-        protected Sentinel $auth
+        protected Sentinel $auth,
+        protected Session $session,
     ) {}
 
     public function index(ServerRequestInterface $request)
@@ -21,7 +25,9 @@ class LoginController
         $response = new Response();
 
         $response->getBody()->write(
-            $this->view->render('auth/login.twig')
+            $this->view->render('auth/login.twig', [
+                'errors' => $this->session->getFlashBag()->get('errors')[0] ?? null
+            ])
         );
 
         return $response;
@@ -29,6 +35,15 @@ class LoginController
 
     public function store(ServerRequestInterface $request)
     {
+        try {
+            v::key('email', v::email()->notEmpty())
+                ->key('password', v::notEmpty())
+                ->assert($request->getParsedBody());
+        } catch (ValidatorException $e) {
+            $this->session->getFlashBag()->add('errors', $e->getMessages());
+            return new Response\RedirectResponse('/login');
+        }
+
         if (!$this->auth->authenticate($request->getParsedBody())) {
             // flash message
             return new Response\RedirectResponse('/login');
