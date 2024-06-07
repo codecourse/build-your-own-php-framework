32bfb1f320fa4ebb1098e247568eeca9341ee4f7
Alex Garrett-Smith <alex@codecourse.com>
2024-06-07 15:19:56 +0100

    Episode 41

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -8,12 +8,16 @@ use Cartalyst\Sentinel\Sentinel;
 use Laminas\Diactoros\Request;
 use Laminas\Diactoros\Response;
 use Psr\Http\Message\ServerRequestInterface;
+use Respect\Validation\Exceptions\ValidatorException;
+use Respect\Validation\Validator as v;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class RegisterController
 {
     public function __construct(
         protected View $view,
-        protected Sentinel $auth
+        protected Sentinel $auth,
+        protected Session $session
     ) {}
 
     public function index(ServerRequestInterface $request)
@@ -21,7 +25,9 @@ class RegisterController
         $response = new Response();
 
         $response->getBody()->write(
-            $this->view->render('auth/register.twig')
+            $this->view->render('auth/register.twig', [
+                'errors' => $this->session->getFlashBag()->get('errors')[0] ?? null
+            ])
         );
 
         return $response;
@@ -29,6 +35,16 @@ class RegisterController
 
     public function store(ServerRequestInterface $request)
     {
+        try {
+            v::key('first_name', v::alpha()->notEmpty())
+                ->key('email', v::email()->notEmpty())
+                ->key('password', v::notEmpty())
+                ->assert($request->getParsedBody());
+        } catch (ValidatorException $e) {
+            $this->session->getFlashBag()->add('errors', $e->getMessages());
+            return new Response\RedirectResponse('/register');
+        }
+
         if ($user = $this->auth->registerAndActivate($request->getParsedBody())) {
             $this->auth->login($user);
         }
